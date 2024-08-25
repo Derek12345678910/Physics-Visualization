962914ca641a3b039a31c38f3029ae90df6673a8
Your Namedwadwadadfwadwadhawhjduwahdgit config --global user.email you@example.comgit config --global user.name Your <drohi2026@gmail.com>
2024-08-24 23:59:06 -0400

    glew library

--- files.h
+++ files.h
@@ -1,7 +1,11 @@
+#ifndef files.h
+#define files.h
+
 #include <iostream>
 
 // Include GLFW Header
 #include <GLFW/glfw3.h>
+#include <GL/glew.h>
 
 // Libaries
 #include <vector>
@@ -10,5 +14,51 @@ using namespace std;
 
 // Functions
 
-// Creates a GLFW Window
-int createWindow();
+// Window class
+class Window {
+private:
+    GLFWwindow* window;
+public:
+    Window();
+    ~Window();
+
+    // Open the window
+    bool initialize();
+
+    // Edit the window
+    void mainLoop();
+};
+
+// Class of drawing system
+class Drawer
+{
+private:
+    GLuint vertexArray;
+    GLuint vertexBuffer;
+    GLuint shaderProgram;
+
+    GLuint compileShader(const char* source, GLenum shaderType);
+    GLuint createShaderProgram(const char* vertexSource, const char* fragmentSource);
+
+public:
+    Drawer();
+    ~Drawer();
+
+    bool initialize();
+    void render();
+    void cleanup();
+};
+
+// Class of the coaster elements
+class Coaster
+{
+private:
+    double startHeight;
+    double endHeight;
+    double objectMass;
+    double length;
+    double friction;
+    // could add more
+};
+
+#endif
